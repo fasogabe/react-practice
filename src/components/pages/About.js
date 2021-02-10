95462460d00437396f3f21de1c0626d2358889c3
fasogabe <omicronesta@gmail.com>
2021-02-09 17:31:38 -0800

    latest commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About this app</h1>
+      <p>App to search GitHub users</p>
+    </div>
+  );
+};
+export default About;
